6a43fdd88be289552a837780dcc46f858dbd7b09
mj <kwj4479@gmail.com>
2025-06-06 17:10:35 +0900

    rename: change directory name

--- src/fastapi_filterdeps/predefined/__init__.py
+++
@@ -1,5 +0,0 @@
-from fastapi_filterdeps.predefined.criteria_tags import TagsFieldCriteria
-
-__all__ = [
-    'TagsFieldCriteria',
-]
---
+++ src/fastapi_filterdeps/specific/__init__.py
@@ -0,0 +1,5 @@
+from fastapi_filterdeps.specific.criteria_tags import TagsFieldCriteria
+
+__all__ = [
+    "TagsFieldCriteria",
+]
